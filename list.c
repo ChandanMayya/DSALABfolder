5c85ae857195ad737770f6d01250ea8e9ae279ce
ChandanMayya <chandanmogral@gmail.com>
2022-04-05 16:47:11 +0530

    List

--- list.c
+++ list.c
@@ -124,8 +124,47 @@ void deletespecified(int ele){
 	}
 	free(temp);
 }
+
+void insertspecified(int ele,int pos){
+	int counter=0,count=0;
+	listptr temp,prev;
+	temp=First;
+	listptr new;
+	new=getnode();
+	new->data=ele;
+	new->link=NULL;
+	/*while(temp->link!=NULL){
+		counter++;
+		temp=temp->link;
+		printf("%d",counter);
+	}*/
+	if(First==NULL && pos==1){
+		new->data=ele;
+		new->link=NULL;
+		First=new;
+	}else{	printf("INside here");
+		printf("Counter:%d\n",counter);
+		while(count!=pos&&temp->link!=NULL){
+			prev=temp;
+			temp=temp->link;
+			count++;
+			printf("%d\t",count);
+		}
+		if(pos==1){
+			new->data=ele;
+			new->link=First;
+			First=new;
+		}
+		if(counter==pos){
+			printf("Final if");
+			new->data=ele;
+			new->link=prev->link;
+			prev->link=new;
+		}
+	}
+}
 void main(){
-	int ch,ele;
+	int ch,ele,pos;
 	while(1){
 		printf("\n1.Insert Begin\t2Insert End\n3.Delete Begin\t4.Delete End\n5.Display\t6Delete Specified\t7.Exit");
 		printf("Enter your choice:");
@@ -157,6 +196,12 @@ void main(){
 			break;
 		case 7:
 			exit(0);
+		case 8:
+			printf("Enter the element to Insert:\t");
+			scanf("%d",&ele);
+			printf("Enter the position to Insert:\t");
+			scanf("%d",&pos);
+			insertspecified(ele,pos);
 		}
 	}
 }
